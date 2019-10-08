8f10c5df3d0b0d136f19cc7db68cde54b799da7d
Andy Bell <andy.bell@barkingmad.io>
2019-10-08 20:02:51 +0200

    Generic property generator

---
+++ contracts/PropertyGenerator.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.5.0;
+import '@openzeppelin/contracts/ownership/Ownable.sol';
+import './IPropertyGenerator.sol';
+
+contract PropertyGenerator is IPropertyGenerator, Ownable {
+
+    constructor() public {
+    }
+
+    function generate(address _sender, uint256 name)
+        external
+        returns (uint256 value) {
+
+        return 1;
+    }
+}
