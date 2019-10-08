7024cbd8fe12b7e7a0446876bc965e4710e5d4f2
Andy Bell <andy.bell@barkingmad.io>
2019-10-08 20:02:24 +0200

    update to @openzeppelin package

--- contracts/IPropertyGenerator.sol
+++ contracts/IPropertyGenerator.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.5.0;
 
 interface IPropertyGenerator {
-    function generate(address _sender, bytes16 name)
+    function generate(address _sender, uint256 name)
     external
     returns (uint256 value);
 }
