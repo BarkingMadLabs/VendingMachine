53a2ad3f7fe2aabaab0c77fee4c741e1ec14c3f8
Andy Bell <andy.bell@barkingmad.io>
2019-10-09 21:33:09 +0200

    remove address for sender

--- contracts/IPropertyGenerator.sol
+++ contracts/IPropertyGenerator.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.5.0;
 
 interface IPropertyGenerator {
-    function generate(address _sender, uint256 name)
+    function generate(uint256 name)
     external
     returns (uint256 value);
 }
