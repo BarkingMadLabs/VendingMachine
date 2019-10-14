fe4b14c6bd5f9cc1a9515ffa7ac1d46fb7c28157
Andy Bell <andy.bell@barkingmad.io>
2019-10-14 20:05:09 +0200

    deploy

--- migrations/2_vendingmachine.js
+++ migrations/2_vendingmachine.js
@@ -1,5 +1,23 @@
-const VendingMachine = artifacts.require("VendingMachine");
+const PropertyNames = artifacts.require("./PropertyNames.sol");
+const PropertyGenerator = artifacts.require("./PropertyGenerator.sol");
+const VendingObject = artifacts.require('./VendingObject.sol');
+const VendingMachine = artifacts.require('./VendingMachine.sol');
+const MetaData = artifacts.require('./MetaData.sol');
 
-module.exports = function(deployer) {
-  deployer.deploy(VendingMachine);
-};
+module.exports = async function(deployer, network, accounts) {
+
+  await deployer.deploy(PropertyNames);
+  await deployer.deploy(PropertyGenerator);
+  await deployer.deploy(MetaData, "http://test.barkingmad.io/");
+  let propertyNamesInstance = await PropertyNames.deployed();
+  let propertyGeneratorInstance = await PropertyGenerator.deployed();
+  let metaDataInstance = await MetaData.deployed();
+  await deployer.deploy(VendingObject, 
+                        metaDataInstance.address, 
+                        propertyNamesInstance.address,  
+                        propertyGeneratorInstance.address);
+  let vendingObjectInstance = await VendingObject.deployed();
+  await deployer.deploy(VendingMachine, vendingObjectInstance.address);
+
+  console.log(VendingMachine.deployed().address);
+}
