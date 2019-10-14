9b19c35a603bd2f82ec0d5ddccdba4e006edd66b
Andy Bell <andy.bell@barkingmad.io>
2019-10-14 20:08:37 +0200

    property generator interface

--- contracts/IPropertyGenerator.sol
+++ contracts/IPropertyGenerator.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.5.0;
 
 interface IPropertyGenerator {
-    function generate(uint256 name)
+    function generate(uint256 _propertyId)
     external
     returns (uint256 value);
 }
