f0f16f08215c1f21881de753a4da06042688a6df
Andy Bell <andy.bell@barkingmad.io>
2019-10-16 20:51:03 +0200

    Add whitelistadminrole, set price for the object and allow excess funds being sent to be withdrawn by the sender at a later point in the future

--- contracts/VendingMachine.sol
+++ contracts/VendingMachine.sol
@@ -1,16 +1,24 @@
 pragma solidity ^0.5.0;
-import '@openzeppelin/contracts/ownership/Ownable.sol';
+import '@openzeppelin/contracts/access/roles/WhitelistAdminRole.sol';
+import '@openzeppelin/contracts/payment/PullPayment.sol';
 import './IVendingObjectCreator.sol';
 
-contract VendingMachine is Ownable {
+contract VendingMachine is WhitelistAdminRole, PullPayment {
 
+    using SafeMath for uint256;
+    
     IVendingObjectCreator public vendingObjectCreator;
 
-    event VendingObjectCreated(address indexed to, uint256 indexed tokenId);
+    event VendingObjectCreated(address indexed to, uint256 indexed tokenId, uint256 priceInWei);
+    event PriceChanged(address indexed sender, uint256 indexed oldPrice, uint256 indexed newPrice);
+    event VendingObjectCreatorChanged(address indexed sender, address indexed oldAddress, address indexed newAddress);
 
-    constructor(IVendingObjectCreator _vendingObjectCreator)
+    uint256 public price;
+
+    constructor(IVendingObjectCreator _vendingObjectCreator, uint256 _price)
         public {
 
+        price = _price;
         // TODO - check the validity of these interfaces
         vendingObjectCreator = _vendingObjectCreator;
     }
@@ -21,19 +29,35 @@ contract VendingMachine is Ownable {
         returns(uint256 _tokenId) {
 
         // check payment
-        require(msg.value > 0.001 ether, 'Send more next time');
+        require(msg.value >= price, 'Send more next time');
         require(msg.sender != address(0x0), 'Invalid address');
 
         _tokenId = vendingObjectCreator.mint(msg.sender);
 
-        emit VendingObjectCreated(msg.sender, _tokenId);
+        emit VendingObjectCreated(msg.sender, _tokenId, price);
+
+        uint256 credit = msg.value.sub(price);
+        if (credit > 0)
+            _asyncTransfer(msg.sender, credit);
 
         return _tokenId;
     }
 
     function setVendingObject(IVendingObjectCreator _vendingObjectCreator)
         public
-        onlyOwner {
+        onlyWhitelistAdmin {
+
+        emit VendingObjectCreatorChanged(msg.sender, address(vendingObjectCreator), address(_vendingObjectCreator));
+
         vendingObjectCreator = _vendingObjectCreator;
+    }
+
+    function setPrice(uint256 _price)
+        public
+        onlyWhitelistAdmin {
+
+        emit PriceChanged(msg.sender, price, _price);
+
+        price = _price;
     }
 }
