51571713e3fded8183dde4e037c65a656646f51b
Andy Bell <andy.bell@barkingmad.io>
2019-10-18 18:47:14 +0200

    test balances of share holders in contract

--- test/vendingmachine.js
+++ test/vendingmachine.js
@@ -9,16 +9,24 @@ contract("Vending Machine", async accounts => {
     let vendingMachineInstance;
     let vendingObjectInstance;
     let tokenId;
-    let pricePerVendingObject = toWei('0.001');
-    let priceToPay = toWei('0.01');
+    let pricePerVendingObjectInWei = toWei('0.001');
+    let priceToPayInWei = toWei('0.01');
+    [admin, account1, account2, shareHolder1, shareHolder2] = accounts;
+    let shareHolders = [shareHolder1, shareHolder2];
+    let shareHolder1Shares = 50;
+    let shareHolder2Shares = 50;
+    
+    let shareAmounts = [shareHolder1Shares, shareHolder2Shares];
 
     beforeEach("prepare some things", async function() {
-        [admin, account1, account2] = accounts;
+        
 
         vendingObjectInstance = await VendingObject.deployed();
 
-        vendingMachineInstance = await VendingMachine.new(  vendingObjectInstance.address, 
-                                                            pricePerVendingObject, 
+        vendingMachineInstance = await VendingMachine.new(  shareHolders,
+                                                            shareAmounts,
+                                                            vendingObjectInstance.address, 
+                                                            pricePerVendingObjectInWei, 
                                                             {from:admin});
         
         vendingObjectInstance.addWhitelistAdmin(vendingMachineInstance.address);
@@ -26,7 +34,7 @@ contract("Vending Machine", async accounts => {
 
     it("should mint when sending value", async function() {
         
-        let txObj = await vendingMachineInstance.mint({from:account1, value:pricePerVendingObject});
+        let txObj = await vendingMachineInstance.mint({from:account1, value:pricePerVendingObjectInWei});
         assert.strictEqual(txObj.receipt.logs.length, 1);
         assert.strictEqual(txObj.logs.length, 1);
         const logTransfer = txObj.logs[0];
@@ -39,7 +47,7 @@ contract("Vending Machine", async accounts => {
   
     it("should mint when sending value and allow me to withdraw the change", async function() {
         
-        let txObj = await vendingMachineInstance.mint({from:account1, value:priceToPay});
+        let txObj = await vendingMachineInstance.mint({from:account1, value:priceToPayInWei});
         assert.strictEqual(txObj.receipt.logs.length, 1);
         assert.strictEqual(txObj.logs.length, 1);
         const logTransfer = txObj.logs[0];
@@ -50,7 +58,7 @@ contract("Vending Machine", async accounts => {
         assert.isAbove(tokenId, 0, 'We need a valid token id');
 
         let balanceBN = await vendingMachineInstance.payments(account1);
-        let calculatedBalanceBN = toBN(priceToPay).sub(toBN(pricePerVendingObject));
+        let calculatedBalanceBN = toBN(priceToPayInWei).sub(toBN(pricePerVendingObjectInWei));
         
         assert.strictEqual(balanceBN.toString(), calculatedBalanceBN.toString(), 'We need to have something left');
 
@@ -77,5 +85,34 @@ contract("Vending Machine", async accounts => {
         let fn = vendingMachineInstance.mint({from:account1, value:toWei('0.0009', 'ether')});
         await truffleAssert.reverts(    fn, 
                                         'Send more next time');
+    });
+
+    it("should split the contract value", async function() {
+        
+        let txObj = await vendingMachineInstance.mint({from:account1, value:pricePerVendingObjectInWei});
+        tokenId = txObj.logs[0].args.tokenId.toNumber();
+        assert.isAbove(tokenId, 0, 'We need a valid token id');
+
+        let balanceOfShareHolder1 = await web3.eth.getBalance(shareHolder1);
+        let balanceOfShareHolder2 = await web3.eth.getBalance(shareHolder2);
+        
+        let txObj1 = await vendingMachineInstance.release(shareHolder1);
+        assert.strictEqual(txObj1.logs[0].event, "PaymentReleased");
+
+        let txObj2 = await vendingMachineInstance.release(shareHolder2);
+        assert.strictEqual(txObj2.logs[0].event, "PaymentReleased");
+
+        let balanceOfShareHolder1After = await web3.eth.getBalance(shareHolder1);
+        let balanceOfShareHolder2After = await web3.eth.getBalance(shareHolder2);
+        
+        let totalShares = shareHolder1Shares + shareHolder2Shares;
+        
+        assert.strictEqual( (toBN(balanceOfShareHolder1).sub(toBN(balanceOfShareHolder1After))).toString(),
+                            (toBN(pricePerVendingObjectInWei).mul(toBN(shareHolder1Shares))).div(toBN(totalShares)), 
+                            'Share holder 1 should get their percentage');
+
+        assert.strictEqual( (toBN(balanceOfShareHolder2).sub(toBN(balanceOfShareHolder2After))).toString(),
+                            (toBN(pricePerVendingObjectInWei).mul(toBN(shareHolder1Shares))).div(toBN(totalShares)), 
+                            'Share holder 2 should get their percentage');
     });
 });
